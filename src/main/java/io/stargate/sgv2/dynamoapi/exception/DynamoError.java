01f50797ba8f7b79e31786dd16dedeacb97e06cf
FacePrintPay <FacePrintPay@users.noreply.github.com>
2026-04-12 09:16:13 +0000

    deploy: 6d5ac896cb6020185f5ca75d769ea582724c9eec

--- src/main/java/io/stargate/sgv2/dynamoapi/exception/DynamoError.java
+++
@@ -1,8 +0,0 @@
-package io.stargate.sgv2.dynamoapi.exception;
-
-public record DynamoError(String __type, String message) {
-  public DynamoError(String __type, String message) {
-    this.__type = __type;
-    this.message = message;
-  }
-}
